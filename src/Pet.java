030feb562fd213e434d33fa45dd18add673e2256
eomjeongho <eomjeongho1@naver.com>
2015-09-17 13:15:23 +0900

    complete

--- src/Pet.java
+++ src/Pet.java
@@ -11,7 +11,9 @@ public class Pet {
 	}
 	
 	public Pet(String initialName, int initialAge, double initialWeight) {
-		 // 구현하시오.
+		 name = initialName;
+		 age = initialAge;
+		 weight = initialWeight;
 	}
         // 오버로딩
 	public Pet(String initialName) {
@@ -35,29 +37,28 @@ public class Pet {
 		weight = 0;
 	}
 	public void set(String newName, int newAge, double newWeight) {
-		 // 구현 하시오.
+		 name = newName;
+		 age = newAge;
+		 weight = newWeight;
 	}
 	
 	public void setAge( int newAge) {
-               // 구현하시오.
+         age = newAge;
 	}
 	
 	public void setWeight( double newWeight) {
-	      // 구현하시오.
+	      weight = newWeight;
        }
 	
 	public String getName() {
-		return null;
-		// 구현하시오.
+		return name;
 	}
 	
 	public int getAge() {
-		return 0;
-		// 구현하시오.
+		return age;
 	}
 	
 	public double getWeight() {
-		return 0;
-		// 구현하시오.
+		return weight;
 	}
 }
